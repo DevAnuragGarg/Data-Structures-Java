4eb00061a4fa2bc5d5a81b45618fc295bad3d46a
anurag.garg <developer.anuraggarg@gmail.com>
2020-09-18 15:46:52 +0530

    Print all Root to leaf paths

--- src/com/anudev/ds/trees/PrintEachRootToLeafPath.java
+++ src/com/anudev/ds/trees/PrintEachRootToLeafPath.java
@@ -1,4 +1,38 @@
 package com.anudev.ds.trees;
 
+import java.util.LinkedList;
+
 public class PrintEachRootToLeafPath {
+
+    public static LinkedList<Node> linkedList = new LinkedList();
+
+    public static void printAllPaths(Node node) {
+        System.out.println("All root to leaf paths: ");
+        findAllRootToLeafPath(node);
+    }
+
+    private static void findAllRootToLeafPath(Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        // add to linked list
+        linkedList.add(node);
+
+        if (node.getLeftNode() == null && node.getRightNode() == null) {
+            printLinkedList();
+        } else {
+            findAllRootToLeafPath(node.getLeftNode());
+            findAllRootToLeafPath(node.getRightNode());
+        }
+        linkedList.removeLast();
+    }
+
+    private static void printLinkedList() {
+        for (Node node : linkedList) {
+            System.out.print(node.getValue() + ",");
+        }
+        System.out.println();
+    }
 }
--- src/com/anudev/ds/trees/TreesMainProgram.java
+++ src/com/anudev/ds/trees/TreesMainProgram.java
@@ -28,28 +28,37 @@ public class TreesMainProgram {
         InOrderTraversal.inOrderTraversal(rootNode);
         PostOrderTraversal.postOrderTraversal(rootNode);
         LevelTraversing.nonRecursiveLevelTraversal(rootNode);
+        System.out.println("\n");
 
         // height of tree
-        System.out.println();
         System.out.println("Height of Tree Recursively:\t\t" + HeightOfBinaryTree.heightOfBinaryTreeRecursively(rootNode));
         System.out.println("Height of Tree Non Recursively:\t" + HeightOfBinaryTree.heightOfBinaryTreeNonRecursively(rootNode));
+        System.out.println();
 
         // size of binary tree
         System.out.println("Size of Tree Recursively:\t\t" + SizeOfBinaryTree.sizeOfBinaryTreeRecursively(rootNode));
         System.out.println("Size of Tree Non Recursively:\t" + SizeOfBinaryTree.sizeOfBinaryTreeNonRecursively(rootNode));
+        System.out.println();
 
         // deepest node of binary tree
         DeepestNodeOfBinaryTree.printDeepestNode(rootNode);
+        System.out.println();
 
         // Number of leaves
         System.out.println("Number of Leaves in tree Recursively:\t\t" + NumberOfLeaves.findNumberOfLeavesRecursively(rootNode));
         System.out.println("Number of Leaves in tree Non Recursively:\t" + NumberOfLeaves.findNumberOfLeavesNonRecursively(rootNode));
-
+        System.out.println();
 
         // diameter of the tree
         System.out.println("Diameter of the tree is: " + DiameterOfTree.diameterOfTree(rootNode));
+        System.out.println();
 
         // vertical sum of the tree
         VerticalSumOfTree.printTheVerticalSum(rootNode);
+        System.out.println();
+
+        // print root to leaf paths
+        PrintEachRootToLeafPath.printAllPaths(rootNode);
+        System.out.println();
     }
 }
