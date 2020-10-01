8188a21e4af2201b54f47fe911398c80e705c70c
anurag.garg <developer.anuraggarg@gmail.com>
2020-10-01 21:35:41 +0530

    DFS implementation

--- src/com/anudev/ds/graphs/DFS.java
+++ src/com/anudev/ds/graphs/DFS.java
@@ -1,5 +1,77 @@
 package com.anudev.ds.graphs;
 
+import java.util.Stack;
 
+/**
+ * DFS works in manner similar to preorder traversal of trees. Internally
+ * algorithm uses stacks. Need to take care of 2 things: 1) visiting a
+ * vertex 2) Exploration of vertex
+ */
 public class DFS {
+
+    private final Vertex[] vertices;
+    private final int[][] adjMatrix;
+    private final Stack<Integer> stack;
+
+    private int vertexCount;
+
+    public DFS(int numberOfVertices) {
+        stack = new Stack<>();
+        adjMatrix = new int[numberOfVertices][numberOfVertices];
+        vertices = new Vertex[numberOfVertices];
+    }
+
+    public void addVertex(char lab) {
+        vertices[vertexCount] = new Vertex(lab);
+        vertexCount++;
+    }
+
+    public void addEdge(int start, int end) {
+        adjMatrix[start][end] = 1;
+        adjMatrix[end][start] = 1;
+    }
+
+    public void performDFS(int value) {
+        stack.push(value);
+        vertices[value].setVisited(true);
+
+        // keep pushing
+        while (!stack.isEmpty()) {
+
+            int v = getAdjUnvisitedMatrix(stack.peek());
+            if (v != -1) {
+                vertices[v].setVisited(true);
+                stack.push(v);
+            } else {
+                stack.pop();
+            }
+        }
+    }
+
+    private int getAdjUnvisitedMatrix(int x) {
+        for (int i = 0; i < vertexCount; i++) {
+            if (adjMatrix[x][i] == 1 && !vertices[i].isVisited()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+class Vertex {
+    private char label;
+    private boolean isVisited;
+
+    public Vertex(char lab) {
+        label = lab;
+    }
+
+    public boolean isVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        System.out.print(label + ", ");
+        isVisited = visited;
+    }
 }
--- src/com/anudev/ds/graphs/GraphMainProgram.java
+++ src/com/anudev/ds/graphs/GraphMainProgram.java
@@ -3,6 +3,8 @@ package com.anudev.ds.graphs;
 public class GraphMainProgram {
 
     public static void main(String[] args) {
+
+        // Graph with adjacency matrix and lists
         Graph graph = new Graph(5);
         graph.addEdge(0, 1);
         graph.addEdge(0, 4);
@@ -14,5 +16,23 @@ public class GraphMainProgram {
         graph.printAdjacencyMatrix();
         System.out.println();
         graph.printAdjacencyList();
+        System.out.println();
+
+        // DFS
+        System.out.println("DFS");
+        DFS dfs = new DFS(4);
+        dfs.addVertex('A');
+        dfs.addVertex('B');
+        dfs.addVertex('C');
+        dfs.addVertex('D');
+        dfs.addEdge(0, 1);
+        dfs.addEdge(0, 2);
+        dfs.addEdge(1, 2);
+        dfs.addEdge(2, 0);
+        dfs.addEdge(2, 3);
+        dfs.addEdge(3, 3);
+        dfs.performDFS(2);
+
+        // BFS
     }
 }
